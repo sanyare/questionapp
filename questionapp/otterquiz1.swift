5987a24d02391a2c74e4d8ccb5c2e1f38768853d
Sanya Reddy <sanyasreddy@gmail.com>
2024-07-30 10:53:33 -0400

    final question app project

---
+++ questionapp/otterquiz1.swift
@@ -0,0 +1,18 @@
+//
+//  otterquiz1.swift
+//  questionapp
+//
+//  Created by Scholar on 7/30/24.
+//
+
+import SwiftUI
+
+struct otterquiz1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    otterquiz1()
+}
